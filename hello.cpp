2e6d9e16d25ed5a6707838d700d988a182645691
Yury Filyanin <yury.filyanin@yandex.com>
2024-10-12 17:45:55 +0700

    used new functions in hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
 #include "getString.h"
 #include "dlib.h"
 #include "slib.h"
 
 int main() {
-    std::string world(getWorld());
-    std::cout << upperFirst(getHello()) << upperFirst(world) << std::endl;
+    std::string hello = upperFirst(getHello()), world = getWorld();
+    printMsg(buildHelloWorldMsg(hello, world));
 }
